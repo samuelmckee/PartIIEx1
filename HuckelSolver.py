98c710934ec8d1a0103a0e134fbbf50432b32292
Sam McKee <samuel.mckee98@gmail.com>
2018-10-04 16:39:58 +0100

    first refactor

--- HuckelSolver.py
+++ HuckelSolver.py
@@ -1,34 +1,19 @@
 import numpy as np
 
-class Molecule :
-  #Number of atoms in molecule
-  num_atoms = 1
-  #Set containing connections between adjacent atoms
-  connections = set()
+n = 10
+alpha = 0.
+beta = -1.
 
-  def __init__(self, n) :
-    # A molecule object must have at least 1 atom
-    if n < 1:
-      raise ValueError('Must enter a positive integer')
-    else:
-      self.num_atoms = n
+#Generate matrix with diagonal elements set to alpha value
+  huckel = np.identity[n]
+  huckel *= alpha
+#Set matrix elements for adjecent atoms equal to beta value
 
-  #Make atoms i and j adjacent
-  def add_adjacent(self,i,j):
-    if i < 0 or j < 0 or i >= self.num_atoms or j >= self.num_atoms: 
-      print('Invalid atoms')
-    elif i < j :
-      self.connections.add((i,j))
-    elif j < i :
-      self.connections.add((j,i))
-    else:
-      # i = j. Atom cannot connect to itself
-      print('Cannot connect atom to itself')
-
-  #Make i and j no longer adjacent
-  def remove_adjacent(self,i,j):
-    if i < j :
-      self.connections.discard( (i,j) )
-    elif j < i :
-      self.connections.discard( (j,i) )
-
+#Make atoms i and j adjacent
+def add_adjacent(huckel, i, j, beta):
+  if i = j :
+    # i = j. Atom cannot connect to itself
+    print('Cannot connect atom to itself')
+  else :
+    huckel[i,j] = beta
+    huckel[j,i] = beta
