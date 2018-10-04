47bbba9df9c33024bfd41cdf4e1532622bf905fb
Sam McKee <samuel.mckee98@gmail.com>
2018-10-04 19:23:05 +0100

    Added functions to generate platonic solids

--- HuckelSolver.py
+++ HuckelSolver.py
@@ -50,4 +50,44 @@ def generate_cyclic(n) :
   m.add_adjacent(0, n-1)
   return m
 
+def generate_tetrahedron():
+  #All atoms connected to all other atoms
+  m = Molecule(4)
+  for i in range(0,4) :
+   for j in range(i,4) :
+    if not i==j :
+      m.add_adjacent(i, j)
+  return m
 
+def generate_cube():
+  #Start with 8 member ring and add links across to form cube
+  m = generate_cyclic(8)
+  m.add_adjacent(0,3)
+  m.add_adjacent(4,7)
+  m.add_adjacent(2,5)
+  m.add_adjacent(1,6)
+  return m
+
+def generate_dodecahedron():
+  #Start with empty 20 point molecule
+  m = Molecule(20)
+  #Dodecahedron is split into planes of atoms, which are added sequentially
+  #Top and bottom layers are 5 member rings
+  for i in range(0,4) :
+    m.add_adjacent(i,i+1)
+    m.add_adjacent(i+15,i+16)
+  m.add_adjacent(0,4)
+  m.add_adjacent(15,19)
+
+  #Connect top and bottom layer to middle layers
+  for i in range(0,5) :
+    m.add_adjacent(i, i+5)
+    m.add_adjacent(i+15, i+10)
+
+  #Connect two middle layers together
+  for i in range(0, 4) :
+    m.add_adjacent(i + 5, i + 10)
+    m.add_adjacent(i + 6, i + 10)
+  m.add_adjacent(5,14)
+  m.add_adjacent(9,14)
+  return m
