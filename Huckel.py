558017a614d76d35122daaf6cdd5d4afd649950f
Sam McKee <samuel.mckee98@gmail.com>
2018-10-04 21:46:00 +0100

    Added function to calculate and display degeneracies

--- Huckel.py
+++ Huckel.py
@@ -1,24 +1,23 @@
 import numpy as np
+import math
 
 class Molecule :
-  #Number of atoms in molecule
-  num_atoms = 1
-  #Set containing connections between adjacent atoms
-  huckel = np.zeros((2,2))
+  num_atoms = 1              #Number of atoms in molecule
+  huckel = np.zeros((2,2))   #Set containing connections between adjacent atoms
+
 
   def __init__(self, n) :
-    # A molecule object must have at least 1 atom
-    if n < 1:
+    if n < 1:      # A molecule object must have at least 1 atom
       raise ValueError('Must enter a positive integer')
     else:
       self.num_atoms = n
       self.huckel = np.zeros((n,n))
+  
   #Make atoms i and j adjacent
   def add_adjacent(self,i,j):
     if i < 0 or j < 0 or i >= self.num_atoms or j >= self.num_atoms: 
       print('Invalid atoms')
     elif i == j :
-      # i = j. Atom cannot connect to itself
       print('Cannot connect atom to itself')
     else :
       self.huckel[i,j] = -1
@@ -58,7 +57,6 @@ def generate_dodecahedron():
   #Start with empty 20 point molecule
   m = Molecule(20)
   #Dodecahedron is split into planes of atoms, which are added sequentially
-  #Top and bottom layers are 5 member rings
   for i in range(0,4) :
     m.add_adjacent(i,i+1)
     m.add_adjacent(i+15,i+16)
@@ -77,3 +75,19 @@ def generate_dodecahedron():
   m.add_adjacent(5,14)
   m.add_adjacent(9,14)
   return m
+
+def print_evals(evals):
+  print("Degeneracy  |  Eigenvalue")
+  print("------------|------------")
+  evals.sort()
+  current_eval = evals[0]  #Eigenvalue currently being checked for degeneracy
+  degeneracy = 1   #Degeneracy of current eigenvalue
+  for eval in evals[1:]:
+    if math.isclose(eval, current_eval, abs_tol=1e-10) :  #Checks if two eigenvalues are within given tolerance
+      degeneracy += 1
+    else :
+      print ("{: 10d}  | {: 2.8f}".format(degeneracy, current_eval))
+      current_eval = eval
+      degeneracy = 1
+  print ("{: 10d}  | {: 2.8f}".format(degeneracy, current_eval))
+
