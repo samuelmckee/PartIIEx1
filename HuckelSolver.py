58bc68ac66d09add06a1e38c078fc8a53f7c876d
Sam McKee <samuel.mckee98@gmail.com>
2018-10-04 17:16:36 +0100

    Added cyclic huckel generator

--- HuckelSolver.py
+++ HuckelSolver.py
@@ -6,8 +6,15 @@ def generate_linear (n, alpha, beta)
   huckel *= alpha
 
   #Set matrix elements for adjacent atoms to beta
-  for i in range(0,n - 1)
+  for i in range(0,n - 1) :
     huckel[i,i+1] = beta
     huckel[i+1,i] = beta
-
   return huckel
+
+def generate_cyclic (n, alpha, beta)
+  #Start out with linear polyene, then join the ends to account for ring 
+  huckel = generate_linear(n, alpha, beta)
+  huckel[0,n-1] = beta
+  huckel[n-1,0] = beta
+  return huckel
+
