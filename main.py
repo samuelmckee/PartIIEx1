5a96de92873829083e76df329fd9f7349d6d8eac
Sam McKee <samuel.mckee98@gmail.com>
2018-10-05 16:32:29 +0100

    Added support for c60

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ while 1 :
   print("3. Tetrahedron")
   print("4. Cube")
   print("5. Dodecahedron")
-  print("6. Exit")
+  print("6. Buckminsterfullerene")
+  print("7. Exit")
 
-  choice = utils.get_int("Enter your selection: ", "Selection must be from list: ", lambda x: x in [1,2,3,4,5,6])
+  choice = utils.get_int("Enter your selection: ", "Selection must be from list: ", lambda x: x in [1,2,3,4,5,6,7])
 
   #lambda functions that return molecule object of desired type
   switcher = {
@@ -19,10 +20,11 @@ while 1 :
       2: lambda: gen.generate_cyclic(utils.get_int("Enter number of atoms in ring: ", "Input must be integer greater than 2: ", lambda x : x > 2)),
       3: lambda: gen.generate_tetrahedron(),
       4: lambda: gen.generate_cube(),
-      5: lambda: gen.generate_dodecahedron()
+      5: lambda: gen.generate_dodecahedron(),
+      6: lambda: gen.generate_c60()
     }
 
-  if choice == 6 :  #User selected exit
+  if choice == 7 :  #User selected exit
     break
   else :
     m = switcher.get(choice)()           #Retrieve and run lambda function from switcher to get molecule object
--- mol_gen.py
+++ mol_gen.py
@@ -33,19 +33,79 @@ def generate_cube():
 def generate_dodecahedron():
   m = mol.Molecule(20)
 
+  #Connections are added starting at top of molecule and working down, 1 plane of atoms at a time
   for i in range(0,4) :
     m.add_adjacent(i,i+1)
-    m.add_adjacent(i+15,i+16)
   m.add_adjacent(0,4)
-  m.add_adjacent(15,19)
 
   for i in range(0,5) :
     m.add_adjacent(i, i+5)
-    m.add_adjacent(i+10, i+15)
 
   for i in range(5, 9) :
     m.add_adjacent(i, i + 5)
     m.add_adjacent(i, i + 6)
   m.add_adjacent(9,10)
   m.add_adjacent(9,14)
+
+  for i in range(10,15) :
+    m.add_adjacent(i, i+5)
+
+  for i in range(15,19) :
+    m.add_adjacent(i, i+1)
+  m.add_adjacent(15,19)
+
   return m
+
+def generate_c60() :
+  m = mol.Molecule(60)
+
+  #Connection are added starting at top of molecule and working down, 1 plane of atoms at a time
+  for i in range(0,4) :
+    m.add_adjacent(i,i+1)
+  m.add_adjacent(0,4)
+
+  for i in range(0,5) :
+    m.add_adjacent(i,i+5)
+
+  for i in range(6,10) :
+    m.add_adjacent(i, 2 * i - 1)
+    m.add_adjacent(i, 2 * i)
+  m.add_adjacent(5,10)
+  m.add_adjacent(5,19)
+
+  for i in range(5,10) :
+    m.add_adjacent(2 * i, 2 * i + 1)
+
+  for i in range(10, 20) :
+    m.add_adjacent(i,i+10)
+
+  for i in range(11,15) :
+    m.add_adjacent(2 * i - 1, 2 * i)
+  m.add_adjacent(20,29)
+
+  for i in range(20,30) : 
+    m.add_adjacent(i, i+10)
+
+  for i in range(15,20) :
+    m.add_adjacent(2 * i, 2 * i + 1)
+
+  for i in range(30,40) :
+    m.add_adjacent(i, i+10)
+
+  for i in range(21,25) :
+    m.add_adjacent(2 * i - 1, 2 * i)
+  m.add_adjacent(40,49)
+
+  for i in range(20,25) :
+    m.add_adjacent(2 * i    , i + 30)
+    m.add_adjacent(2 * i + 1, i + 30)
+
+  for i in range(50,55) :
+    m.add_adjacent(i, i+5)
+
+  for i in range(55,59) :
+    m.add_adjacent(i, i+1)
+  m.add_adjacent(55, 59)
+
+  return m
+
